0208912161e38b23f41cc1c847625efa717e2517
Omar Dacca <omar_dacca@hotmail.com>
2018-03-24 03:12:18 +0300

    update1

--- AltaarefWebAPI/Controllers/StudyGroupsController.cs
+++ AltaarefWebAPI/Controllers/StudyGroupsController.cs
@@ -29,8 +29,8 @@ namespace AltaarefWebAPI.Controllers
         }
 
         // GET: api/StudyGroups/5
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetStudyGroup([FromRoute] int id)
+        [HttpGet("{CourseId}")]
+        public async Task<IActionResult> GetStudyGroup([FromRoute] int CourseId)
         {
             if (!ModelState.IsValid)
             {
