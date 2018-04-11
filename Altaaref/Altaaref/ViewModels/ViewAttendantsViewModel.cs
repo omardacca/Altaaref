550230ff54fdfcc390b2064bfc08bc8898f00fa8
Omar Dacca <omar_dacca@hotmail.com>
2018-04-11 15:08:28 +0300

    fix return of GetAttendants

--- Altaaref/Altaaref/ViewModels/ViewAttendantsViewModel.cs
+++ Altaaref/Altaaref/ViewModels/ViewAttendantsViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Altaaref.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
@@ -10,11 +12,52 @@ namespace Altaaref.ViewModels
         private HttpClient _client = new HttpClient();
         private readonly IPageService _pageService;
 
+        private List<Student> _studentList;
+        public List<Student> StudentsList
+        {
+            get { return _studentList; }
+            private set
+            {
+                _studentList = value;
+                OnPropertyChanged(nameof(StudentsList));
+            }
+        }
+
+        private int StudyGroupId;
+
+        private bool _busy;
+        public bool Busy
+        {
+            get { return _busy; }
+            set
+            {
+                SetValue(ref _busy, value);
+            }
+        }
+
         public ViewAttendantsViewModel(IPageService pageService, int StudyGroupId)
         {
             _pageService = pageService;
+            this.StudyGroupId = StudyGroupId;
+            Init();
         }
 
+        public void Init()
+        {
+            InitStudentList();
+        }
+
+        private async void InitStudentList()
+        {
+            Busy = true;
+            string url = "https://altaarefapp.azurewebsites.net/api/StudyGroupAttendants/GetNames/" + StudyGroupId;
+
+            string content = await _client.GetStringAsync(url);
+            var list = JsonConvert.DeserializeObject<List<Student>>(content);
+            
+            StudentsList = new List<Student>(list);
+            Busy = false;
+        }
 
     }
 }
--- AltaarefWebAPI/Controllers/StudyGroupAttendantsController.cs
+++ AltaarefWebAPI/Controllers/StudyGroupAttendantsController.cs
@@ -49,7 +49,7 @@ namespace AltaarefWebAPI.Controllers
 
         // GET: api/StudyGroupAttendants/5
         [HttpGet("GetNames/{StudyGroupId}")]
-        public async Task<IActionResult> GetAttendantsNames([FromRoute] int StudyGroupId)
+        public async Task<IActionResult> GetAttendants([FromRoute] int StudyGroupId)
         {
             if (!ModelState.IsValid)
             {
@@ -57,7 +57,7 @@ namespace AltaarefWebAPI.Controllers
             }
 
             var studyGroupAttendants = _context.StudyGroupAttendants
-                .Where(sga => sga.StudyGroupId == StudyGroupId).Select(sga => sga.Student.FullName);
+                .Where(sga => sga.StudyGroupId == StudyGroupId).Select(sga => sga.Student);
 
             if (studyGroupAttendants == null)
             {
