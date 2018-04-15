93f8fbf27b6f6bb42a809f31863091cd52f175a6
Omar Dacca <omar_dacca@hotmail.com>
2018-04-15 15:31:00 +0300

    updated database with HelpRequestComment

---
+++ AltaarefWebAPI/Migrations/20180415122839_addHelpRequestComment.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace AltaarefWebAPI.Migrations
+{
+    public partial class addHelpRequestComment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HelpRequestComment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Comment = table.Column<string>(nullable: false),
+                    HelpRequestId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HelpRequestComment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HelpRequestComment_HelpRequest_HelpRequestId",
+                        column: x => x.HelpRequestId,
+                        principalTable: "HelpRequest",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HelpRequestComment_HelpRequestId",
+                table: "HelpRequestComment",
+                column: "HelpRequestId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HelpRequestComment");
+        }
+    }
+}
