203a32c7baaf7bd05f31c922e0d1c14fff91e495
Omar Dacca <omar_dacca@hotmail.com>
2018-04-16 22:24:18 +0300

    Add migration

---
+++ AltaarefWebAPI/Migrations/20180416185732_AddedDateToHelpRequestTable.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace AltaarefWebAPI.Migrations
+{
+    public partial class AddedDateToHelpRequestTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "HelpRequest",
+                nullable: false,
+                defaultValue: DateTime.Today.Date);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "HelpRequest");
+        }
+    }
+}
