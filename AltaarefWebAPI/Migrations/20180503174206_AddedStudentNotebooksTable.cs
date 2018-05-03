9150ce70c35b60f90c256972da6e176063d729e5
Omar Dacca <omar_dacca@hotmail.com>
2018-05-03 20:48:53 +0300

    Migrated

---
+++ AltaarefWebAPI/Migrations/20180503174206_AddedStudentNotebooksTable.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace AltaarefWebAPI.Migrations
+{
+    public partial class AddedStudentNotebooksTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StudentNotebooks",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(nullable: false),
+                    NotebookId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentNotebooks", x => new { x.StudentId, x.NotebookId });
+                    table.ForeignKey(
+                        name: "FK_StudentNotebooks_Notebook_NotebookId",
+                        column: x => x.NotebookId,
+                        principalTable: "Notebook",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StudentNotebooks_Student_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Student",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentNotebooks_NotebookId",
+                table: "StudentNotebooks",
+                column: "NotebookId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentNotebooks");
+        }
+    }
+}
