684badb65fc4f17e9a2468cc5d85b6b9abdb2c67
Omar Dacca <omar_dacca@hotmail.com>
2018-05-21 17:18:14 +0300

    gh

--- AltaarefWebAPI/Controllers/NotificationController.cs
+++ AltaarefWebAPI/Controllers/NotificationController.cs
@@ -22,7 +22,7 @@ namespace AltaarefWebAPI.Controllers
     {
         [HttpPost]
         [Route("topic")]
-        public async string SendMessage()
+        public string SendMessage()
         {
             string serverKey = "AAAAnO7dP3I:APA91bEfzkmagwS55b1SpnE8YI_Qn8Hks3prHWhtk3x_OTZ6vLyWDpzH8mPMnDkpahGKxU66wuUSWqe0UCvC_Bn6z3tRkSwXKDafhtkZDbmWQt2AjHlz8VbTINN5XqSogzRiFroz58cl";
 
